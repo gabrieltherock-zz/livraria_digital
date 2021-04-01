cea11ee0d670cfd4b771acbdac663bc6bb736a19
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-03-31 22:41:07 -0300

    pagina inicial

--- src/main/java/com/gabriel/livrariadigital/controller/LivroController.java
+++ src/main/java/com/gabriel/livrariadigital/controller/LivroController.java
@@ -1,8 +1,13 @@
 package com.gabriel.livrariadigital.controller;
 
+import com.gabriel.livrariadigital.model.Livro;
 import com.gabriel.livrariadigital.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class LivroController {
@@ -10,4 +15,10 @@ public class LivroController {
     @Autowired
     LivroService service;
 
+    @GetMapping
+    public String todosOsLivros(Model model) {
+        List<Livro> livros = service.findAll();
+        model.addAttribute("livros", livros);
+        return "index";
+    }
 }
